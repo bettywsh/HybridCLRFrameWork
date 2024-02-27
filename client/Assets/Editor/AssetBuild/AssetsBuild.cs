7b95bb048b406cc155c39f2154a55da13697d45a
wangshihao <haohao6992@dingtalk.com>
2024-02-27 09:49:07 +0800

    项目调整

--- client/Assets/Editor/AssetBuild/AssetsBuild.cs
+++ client/Assets/Editor/AssetBuild/AssetsBuild.cs
@@ -1,23 +1,46 @@
+using HybridCLR.Editor.Commands;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 using YooAsset.Editor;
 
-public class AssetsBuild 
+public class AssetsBuild
 {
+    public static List<string> AssembliesPostIl2CppStrip = new List<string>{"mscorlib.dll", "System.Core.dll", "System.dll" };
+    public static List<string> HotUpdateDlls = new List<string> { "Hotfix.dll" };
+
     [MenuItem("Build/BuildAndroidAsset", false, 1)]
     public static void BuildAndroid()
     {
+        Debug.Log($"开始构建 : 华佗dll");
+        PrebuildCommand.GenerateAll();
+        AssetDatabase.Refresh();
+        string SourceDir = $"{Application.dataPath}/../HybridCLRData/HotUpdateDlls/{BuildTarget.Android.ToString()}/";
+        string TargetDir = $"{Application.dataPath}/App/Dll/";
+        foreach (string file in HotUpdateDlls)
+        {
+            File.Copy(SourceDir+file, TargetDir+file+ ".bytes",true);
+        }
+
+        SourceDir = $"{Application.dataPath}/../HybridCLRData/AssembliesPostIl2CppStrip/{BuildTarget.Android.ToString()}/";
+        foreach (string file in AssembliesPostIl2CppStrip)
+        {
+            File.Copy(SourceDir + file, TargetDir + file + ".bytes", true);
+        }
+        AssetDatabase.Refresh();
+
         Debug.Log($"开始构建 : {BuildTarget.Android}");
         AppConfig appConfig = AssetDatabase.LoadAssetAtPath<AppConfig>("Assets/Resources/AppConfig.asset");
-        appConfig.AppVersion = Application.version;
-        EditorUtility.SetDirty(appConfig);
-        AssetDatabase.SaveAssets();
-        AssetDatabase.Refresh();
+        //appConfig.AppVersion = Application.version;
+        //EditorUtility.SetDirty(appConfig);
+        //AssetDatabase.SaveAssets();
+        //AssetDatabase.Refresh();
 
         var buildoutputRoot = AssetBundleBuilderHelper.GetDefaultBuildOutputRoot();
         var streamingAssetsRoot = AssetBundleBuilderHelper.GetStreamingAssetsRoot();
+        Debug.LogError(buildoutputRoot);
         // 构建参数
         BuiltinBuildParameters buildParameters = new BuiltinBuildParameters();
         buildParameters.BuildOutputRoot = buildoutputRoot;
@@ -45,5 +68,10 @@ public class AssetsBuild
         {
             Debug.LogError($"构建失败 : {buildResult.ErrorInfo}");
         }
+        AssetDatabase.Refresh();
+
+        Debug.Log($"写入版本文件");
+        string verPath = $"{buildoutputRoot}/{BuildTarget.Android.ToString()}/DefaultPackage/{appConfig.ResVersion}/ver.txt";
+        File.WriteAllText(verPath, Application.version);
     }
 }

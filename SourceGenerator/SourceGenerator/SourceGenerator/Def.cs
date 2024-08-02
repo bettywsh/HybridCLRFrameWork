7d30519c722a18ada13b2d4c925e5e931249bf29
wangshihao <haohao6992@dingtalk.com>
2024-08-02 09:59:53 +0800

    项目调整

---
+++ SourceGenerator/SourceGenerator/SourceGenerator/Def.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SourceGenerator
+{
+    internal class Def
+    {
+        public const string Declaration = "";
+    }
+}
---
+++ SourceGenerator/SourceGenerator/SourceGenerator/UISourceGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.CodeAnalysis;
+
+namespace SourceGenerator
+{
+    [Generator]
+    public class UISourceGenerator : ISourceGenerator
+    {
+        /// <inheritdoc>
+        public void Execute(GeneratorExecutionContext context)
+        {
+            //string moduleName = context.Compilation.SourceModule.Name;
+            //context.AddSource("", )
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+
+        }
+    }
+
+}
+
+file class SyntaxReceiver : ISyntaxReceiver
+{
+    public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+    {
+        throw new NotImplementedException();
+    }
+}
+
